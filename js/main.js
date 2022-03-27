c97585bc2a26efc8cdbfe6b3669f9f912ceb7c32
Dhairya Marwah <85454844+DhairyaMarwah@users.noreply.github.com>
2022-03-27 19:18:16 +0530

    whole structure completed

---
+++ js/main.js
@@ -0,0 +1,62 @@
+const myslide = document.querySelectorAll('.myslide'),
+	  dot = document.querySelectorAll('.dot');
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 8000);
+function autoSlide() {
+	counter += 1;
+	slidefun(counter);
+}
+function plusSlides(n) {
+	counter += n;
+	slidefun(counter);
+	resetTimer();
+}
+function currentSlide(n) {
+	counter = n;
+	slidefun(counter);
+	resetTimer();
+}
+function resetTimer() {
+	clearInterval(timer);
+	timer = setInterval(autoSlide, 8000);
+}
+
+function slidefun(n) {
+	
+	let i;
+	for(i = 0;i<myslide.length;i++){
+		myslide[i].style.display = "none";
+	}
+	for(i = 0;i<dot.length;i++) {
+		dot[i].className = dot[i].className.replace(' active', '');
+	}
+	if(n > myslide.length){
+	   counter = 1;
+	   }
+	if(n < 1){
+	   counter = myslide.length;
+	   }
+	myslide[counter - 1].style.display = "block";
+	dot[counter - 1].className += " active";
+}
+
+
+
+
+
+
+
+// window.addEventListener('scroll',function(){
+// 	const parallax=this.document.querySelector('.parallax-img')
+// 	let scrollPosition=window.pageYOffset;
+// 	parallax.style.transform='translateY('+scrollPosition*.5+'px'
+// })
+// window.addEventListener('scroll',function(){
+// 	const parallax=this.document.querySelector('.parallax-text-part')
+// 	let scrollPosition=window.pageYOffset;
+// 	parallax.style.transform='translateY('+scrollPosition*.2+'px'
+// })
+
+ 

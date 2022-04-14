dd9380ca0c0e24185f61a8487e04ed76f5cd7400
DHAIRYA <dhairya@DHAIRYAs-MacBook-Air.local>
2022-04-14 19:07:08 +0530

    update

---
+++ js/card.js
@@ -0,0 +1,23 @@
+
+const shareButton = document.querySelectorAll("button.shareButton")
+for(i=1;i<shareButton.length;i++){
+
+	shareButton[0].addEventListener("click", (e) => {
+		for( let i=0; i < shareButton.length; i++ ) {
+			shareButton[i].classList.toggle("open")
+			shareButton[0].classList.remove("sent")
+		}
+	})
+}
+
+for( let i=1; i < shareButton.length; i++ ) {
+   
+   shareButton[i].addEventListener("click", (e) => {
+      
+   for( let i=0; i < shareButton.length; i++ ) {
+      shareButton[i].classList.toggle("open")
+   }
+   shareButton[0].classList.toggle("sent")
+   })
+}
+
